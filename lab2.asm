b27f520f018784125849fba43c2fb221a1b43209
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-09-12 13:42:19 -0400

    Add files via upload

--- lab2.asm
+++ lab2.asm
@@ -7,7 +7,7 @@
 # A program to read 2 integer numbers and print out the larger one
 ##############################################
 .data #Data section
-Prompt1: .asciiz "For addition enter 1 \nFor subtraction enter 2 \nFor multiplication enter 3 \nFor and enter 4 \nFor or enter 5"
+Prompt1: .asciiz "For addition enter 1 \nFor subtraction enter 2 \nFor multiplication enter 3 \nFor and enter 4 \nFor or enter 5\n"
 
 .globl main
 .text #Code section
@@ -21,11 +21,9 @@ li $v0, 5 #system call code for Read Integer
 syscall #reads the value of 1st integer into $v0
 move $t0, $v0 #move the 1st integer to $t0
 
-li $v0, 4
-la $a0, $t0
+li $v0,1
+move $a0, $t0
 syscall
-
-
 
 li $v0, 10 #terminate program run and
 syscall #return control to the system
